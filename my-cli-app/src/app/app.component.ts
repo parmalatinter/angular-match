ca11342d4e81f21d3761f485371b6c024bac0874
parmalatinter <parmalatinter@gmail.com>
2017-05-11 16:17:07 +0000

    add compornent

--- my-cli-app/src/app/app.component.ts
+++ my-cli-app/src/app/app.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 import { MessageService } from './message.service';
+import { PostsService } from './posts.service';
 
 @Component({
-  providers: [MessageService],
+  providers: [MessageService, PostsService],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private service: MessageService) { }
+  constructor(private service: MessageService, private postsService: PostsService) { }
   message: string;
   title = 'app works!';
+  posts: any = [];
+  
   ngOnInit() {
     this.message = this.service.getMessage();
+    // Retrieve posts from the API
+    this.postsService.getAllPosts().subscribe(posts => {
+      this.posts = posts;
+    });
   }
 }
