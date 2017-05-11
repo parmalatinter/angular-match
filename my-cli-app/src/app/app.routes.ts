cb09428e1f617934635fdd07b4edce57d82da8e5
parmalatinter <parmalatinter@gmail.com>
2017-05-11 14:55:06 +0000

    add express

--- my-cli-app/src/app/app.routes.ts
+++ my-cli-app/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 
+import {HelloComponent} from './hello/hello.component'
 import {IndexComponent} from './index/index.component'
 import {MypageComponent} from './mypage/mypage.component'
 
+
 export const routes: Routes = [
+  
   { path: '', component: IndexComponent },
+  { path: 'hello', component: HelloComponent },
   { path: 'index', component: IndexComponent },
   { path: 'mypage', component: MypageComponent }
 ];
